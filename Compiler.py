dc74cec3095c2dd090f17a2a4a3f53e90475cc80
Ryan Mason <rmason1@imsa.edu>
2026-02-05 12:42:10 -0600

    Added jump commands and true or false conversions.

--- Compiler.py
+++ Compiler.py
@@ -1,6 +1,8 @@
 import math
 from pathlib import Path
 import sys
+# 13 - 15 are temperaruy registers
+# 0 - 2 are for this, that, pointer
 
 operations = {
     "add" : "+",
@@ -13,9 +15,38 @@ operations = {
 
 segments ={
     "argument" : "ARG",
+    "local" : "LCL",
+    "static" : "16", # base address of static segment
     "this" : "THIS",
     "that" : "THAT",
-    "local" : "LCL",
-    "temp" : "5",
-    "pointer" : "3",
+    "pointer" : "3", # base address of pointer segment
+    "temp" : "5" # base address of temp segment
 }
+
+push = "@SP\nA=M\nM=D\n" + "@SP\nM=M+1\n" # push D onto stack
+true = "D=M-D\nM=-1\n" # set to true
+false = "@SP\nA=M-1\nM=0\n" # set to false
+eq = "D;JEQ\n" # jump if equal
+lt = "D;JLT\n" # jump if less than
+gt = "D;JGT\n" # jump if greater than
+
+#This one is wrong fix it
+# popDReg = "@SP\nAM=M-1\nD=M\n" # pop stack to D
+
+getMem = "@SP\nA=M-1\n"
+
+def conditionalJump(jumpType):
+    jumpCode = ""
+    if jumpType == "eq":
+        jumpCode = eq
+    elif jumpType == "lt":
+        jumpCode = lt
+    elif jumpType == "gt":
+        jumpCode = gt
+    return jumpCode
+
+def main():
+    print(push)
+
+if __name__=="__main__":
+    main()
